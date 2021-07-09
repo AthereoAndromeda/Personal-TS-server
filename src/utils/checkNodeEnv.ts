9a9ea7c1d44785b717a70d4ff230895d4e7b6e60
Angelo <70970878+redstripez08@users.noreply.github.com>
2021-07-09 20:51:29 +0800

    Added checkNodeEnv and utils folder

---
+++ src/utils/checkNodeEnv.ts
@@ -0,0 +1,8 @@
+/**
+ * Checks for Node Environment
+ * @param env Node Environment
+ * @returns
+ */
+export function checkNodeEnv(env: NodeJS.ProcessEnv["NODE_ENV"]): boolean {
+	return env === process.env.NODE_ENV;
+}
