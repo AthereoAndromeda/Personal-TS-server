3ef7e8b5aa6b4d5481a3c0b5ebd52665512d0ef3
Angelo <70970878+redstripez08@users.noreply.github.com>
2021-07-09 18:24:40 +0800

    Root Route

---
+++ src/routes/root.ts
@@ -0,0 +1,14 @@
+import { FastifyPluginCallback } from "fastify";
+
+const route: FastifyPluginCallback = (app, opts, done) => {
+	app.get("/", (req, res) => {
+		res.status(200).send("test");
+	});
+
+	done();
+};
+
+export default {
+	path: "/",
+	route,
+};
