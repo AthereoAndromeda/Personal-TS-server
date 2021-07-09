d9919d2414c4bffd8850429ead0f1aefaf058ec2
Angelo <70970878+redstripez08@users.noreply.github.com>
2021-07-09 18:24:31 +0800

    Added dynamic importing for routes

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
 import fastify, { FastifyInstance } from "fastify";
+import fs from "fs";
+import path from "path";
+import { Route } from "typings";
 
 const app = fastify({
 	logger: {
@@ -6,11 +9,26 @@ const app = fastify({
 	},
 });
 
+/**
+ * Start the Server
+ * @param app
+ */
 async function start(app: FastifyInstance) {
 	try {
 		const { PORT } = process.env;
-
 		if (!PORT) throw "Port Not Found";
+
+		const routeFiles = (
+			await fs.promises.readdir(path.resolve(__dirname, "./routes/"))
+		).filter(file => /\.js$|\.ts$/.test(file));
+
+		for (const routeFile of routeFiles) {
+			const { path, route }: Route = (
+				await import(`./routes/${routeFile}`)
+			).default;
+
+			app.register(route, { prefix: path });
+		}
 
 		await app.listen(PORT);
 		console.log(`Listening to port ${PORT}`);
