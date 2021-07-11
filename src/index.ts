bb0a4f834fa8fcc198b449b257900860e603e2b6
Angelo <70970878+redstripez08@users.noreply.github.com>
2021-07-11 23:30:12 +0800

    Import server and start it

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,6 @@
 import fastify, { FastifyInstance } from "fastify";
-import fs from "fs";
-import path from "path";
-import gqlSchema from "./graphql";
-import fastifyCors from "fastify-cors";
-import mercurius from "mercurius";
+import buildServer from "./server";
 import prisma from "./schema/PrismaClient";
-import { Route } from "typings";
 import { checkNodeEnv } from "./utils";
 
 const app = fastify({
@@ -14,66 +9,21 @@ const app = fastify({
     },
 });
 
-const ctx = {
-    db: prisma,
-};
-
-async function registerPlugins(app: FastifyInstance) {
-    await app.register(mercurius, {
-        schema: gqlSchema,
-        graphiql: "playground",
-        context: () => ctx,
-    });
-
-    await app.register(fastifyCors, {
-        origin: "*",
-    });
-
-    if (checkNodeEnv("development")) {
-        const Altair = (await import("altair-fastify-plugin")).default;
-
-        await app.register(Altair, {
-            path: "/altair",
-        });
-    }
-}
-
-async function registerRoutes(app: FastifyInstance) {
-    // Checks for file that ends in `.ts` or `.js`
-    const validFileRegex = /\.js$|\.ts$/;
-
-    const FilesInRoutes = await fs.promises.readdir(
-        path.resolve(__dirname, "./routes/")
-    );
-
-    const routeFiles = FilesInRoutes.filter(file => validFileRegex.test(file));
-
-    for (const routeFile of routeFiles) {
-        const routeImport = await import(`./routes/${routeFile}`);
-        const { path, route }: Route = routeImport.default;
-
-        app.register(route, { prefix: path });
-    }
-}
-
 /**
- * Start the Server
- * @param app App instance
+ * Starts the Server
+ * @param app Fastify App
  */
 async function start(app: FastifyInstance) {
     try {
         const { PORT } = process.env;
         if (!PORT) throw "Port Not Found";
 
-        await prisma.$connect();
-        await registerPlugins(app);
-        await registerRoutes(app);
+        const server = await buildServer(app, { prisma });
 
-        await app.listen(PORT, "0.0.0.0");
-        console.log(`Listening to port ${PORT}`);
-    } catch (err) {
-        app.log.error(err);
-        // console.error(err);
+        await server.listen(PORT, "0.0.0.0");
+    } catch (error) {
+        app.log.error(error);
+        app.close();
     }
 }
 
