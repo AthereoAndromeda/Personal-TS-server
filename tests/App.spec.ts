a18da9992d59e3b8f73f5fa7105956ea9545b769
Angelo <70970878+redstripez08@users.noreply.github.com>
2021-07-12 04:49:21 +0800

    Added Endpoint Testing

--- tests/App.spec.ts
+++ tests/App.spec.ts
@@ -11,7 +11,7 @@ describe("Test Authorization", () => {
         app1 = await buildServer(fastify(), { prisma: prisma1 });
         await app1.listen(8081, "0.0.0.0");
         return Promise.resolve();
-    });
+    }, 300000);
 
     // Stop Server and disconnect DB
     afterAll(async () => {
@@ -19,7 +19,7 @@ describe("Test Authorization", () => {
         await prisma1.$disconnect();
 
         return Promise.resolve();
-    });
+    }, 300000);
 
     it("Should Return 401 Unauthorized: Incorrect Auth Key", async () => {
         const res = await app1.inject({
@@ -56,6 +56,69 @@ describe("Test Authorization", () => {
         });
 
         expect(res.statusCode).toEqual(200);
+
+        return Promise.resolve();
+    });
+});
+
+let endpointApp: FastifyInstance;
+let endpointPrisma = prisma;
+
+describe("Hit Endpoints for Any Response", () => {
+    beforeAll(async () => {
+        endpointApp = await buildServer(fastify(), { prisma: prisma1 });
+        await endpointApp.listen(8010, "0.0.0.0");
+        return Promise.resolve();
+    }, 300000);
+
+    // Stop Server and disconnect DB
+    afterAll(async () => {
+        await endpointApp.close();
+        await endpointPrisma.$disconnect();
+        return Promise.resolve();
+    }, 300000);
+
+    it("Hit /", async () => {
+        const res = await endpointApp.inject({
+            method: "GET",
+            url: "/",
+            headers: {
+                authorization: process.env.SERVER_AUTHKEY,
+            },
+        });
+
+        expect(res.statusCode).not.toEqual(404);
+        expect(res.payload).toEqual(expect.any(String));
+
+        return Promise.resolve();
+    });
+
+    it("Hit /verse", async () => {
+        const res = await endpointApp.inject({
+            method: "GET",
+            url: "/verse",
+            headers: {
+                authorization: process.env.SERVER_AUTHKEY,
+            },
+        });
+
+        expect(res.statusCode).not.toEqual(404);
+        expect(res.payload).toEqual(expect.anything());
+
+        return Promise.resolve();
+    });
+
+    it("Hit /graphql", async () => {
+        const res = await endpointApp.inject({
+            method: "POST",
+            url: "/graphql",
+            headers: {
+                authorization: process.env.SERVER_AUTHKEY,
+            },
+        });
+
+        expect(res.statusCode).not.toEqual(404);
+        expect(res.payload).toEqual(expect.anything());
 
         return Promise.resolve();
     });
