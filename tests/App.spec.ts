e1f5eb29d86382bd3f5d87c6c9eeca7f0a41b8b7
Angelo <70970878+redstripez08@users.noreply.github.com>
2021-07-12 03:19:51 +0800

    Moved GraphQL test to own file

--- tests/App.spec.ts
+++ tests/App.spec.ts
@@ -1,15 +1,11 @@
 import buildServer from "../src/server";
 import fastify, { FastifyInstance } from "fastify";
 import prisma from "../src/schema/PrismaClient";
-import * as gql from "gql-query-builder";
 import dotenv from "dotenv";
 dotenv.config();
 
-// Multiple app instances required due to parallel nature of Jest
 let app1: FastifyInstance;
-let app2: FastifyInstance;
 let prisma1 = prisma;
-let prisma2 = prisma;
 
 describe("Test Authorization", () => {
     // Start Server and connect to DB
@@ -62,56 +58,6 @@ describe("Test Authorization", () => {
         });
 
         expect(res.statusCode).toEqual(200);
-
-        return Promise.resolve();
-    });
-});
-
-describe("Test GraphQL Endpoint", () => {
-    // Start Server and connect to DB
-    beforeAll(async () => {
-        app2 = await buildServer(fastify(), { prisma: prisma2 });
-        await app2.listen(8083, "0.0.0.0");
-        return Promise.resolve();
-    });
-
-    // Stop Server and disconnect DB
-    afterAll(async () => {
-        await app2.close();
-        await prisma2.$disconnect();
-
-        return Promise.resolve();
-    });
-
-    it("Query: Get All Verses", async () => {
-        const payload = gql.query({
-            operation: "verse",
-            fields: ["id", "title", "content"],
-        });
-
-        const res = await app2.inject({
-            method: "POST",
-            url: "/graphql",
-            headers: {
-                Authorization: process.env.SERVER_AUTHKEY,
-            },
-            payload,
-        });
-
-        const expected = {
-            data: {
-                verse: expect.arrayContaining([
-                    expect.objectContaining({
-                        id: expect.any(Number),
-                        content: expect.any(String),
-                        title: expect.any(String),
-                    }),
-                ]),
-            },
-        };
-
-        expect(res.statusCode).toEqual(200);
-        expect(JSON.parse(res.payload)).toEqual(expected);
 
         return Promise.resolve();
     });
---
+++ tests/GraphQL.spec.ts
@@ -0,0 +1,58 @@
+import buildServer from "../src/server";
+import fastify, { FastifyInstance } from "fastify";
+import prisma from "../src/schema/PrismaClient";
+import * as gql from "gql-query-builder";
+import dotenv from "dotenv";
+dotenv.config();
+
+let app2: FastifyInstance;
+
+describe("Test GraphQL Endpoint", () => {
+    // Start Server and connect to DB
+    beforeAll(async () => {
+        app2 = await buildServer(fastify(), { prisma });
+        await app2.listen(8083, "0.0.0.0");
+        return Promise.resolve();
+    });
+
+    // Stop Server and disconnect DB
+    afterAll(async () => {
+        await app2.close();
+        await prisma.$disconnect();
+
+        return Promise.resolve();
+    });
+
+    it("Query: Get All Verses", async () => {
+        const payload = gql.query({
+            operation: "verse",
+            fields: ["id", "title", "content"],
+        });
+
+        const res = await app2.inject({
+            method: "POST",
+            url: "/graphql",
+            headers: {
+                Authorization: process.env.SERVER_AUTHKEY,
+            },
+            payload,
+        });
+
+        const expected = {
+            data: {
+                verse: expect.arrayContaining([
+                    expect.objectContaining({
+                        id: expect.any(Number),
+                        content: expect.any(String),
+                        title: expect.any(String),
+                    }),
+                ]),
+            },
+        };
+
+        expect(res.statusCode).toEqual(200);
+        expect(JSON.parse(res.payload)).toEqual(expected);
+
+        return Promise.resolve();
+    });
+});
