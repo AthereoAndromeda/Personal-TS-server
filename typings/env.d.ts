a574b35aa49d99cb0287522dbab322f3f6542210
Angelo <70970878+redstripez08@users.noreply.github.com>
2021-07-12 01:32:54 +0800

    Added Server Auth Key

--- typings/env.d.ts
+++ typings/env.d.ts
@@ -3,6 +3,7 @@ declare global {
         interface ProcessEnv {
             PORT?: string;
             NODE_ENV?: "development" | "production";
+            SERVER_AUTHKEY?: string;
         }
     }
 }
