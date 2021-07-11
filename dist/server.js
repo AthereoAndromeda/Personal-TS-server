a398f1b2e8ed9f88ce63ea7bd18f4b8a3787ca19
Angelo <70970878+redstripez08@users.noreply.github.com>
2021-07-12 03:20:39 +0800

    Build v1.0.2-hotfix-1

--- dist/server.js
+++ dist/server.js
@@ -22,13 +22,14 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const fs_1 = __importDefault(require("fs"));
+const promises_1 = __importDefault(require("fs/promises"));
 const path_1 = __importDefault(require("path"));
 const graphql_1 = __importDefault(require("./graphql"));
 const fastify_cors_1 = __importDefault(require("fastify-cors"));
 const mercurius_1 = __importDefault(require("mercurius"));
 const utils_1 = require("./utils");
 const fastify_graceful_shutdown_1 = __importDefault(require("fastify-graceful-shutdown"));
+const middie_1 = __importDefault(require("middie"));
 async function registerPlugins(app, opts) {
     const ctx = {
         db: opts.prisma,
@@ -38,6 +39,7 @@ async function registerPlugins(app, opts) {
         graphiql: "playground",
         context: () => ctx,
     });
+    await app.register(middie_1.default);
     app.register(fastify_graceful_shutdown_1.default);
     await app.register(fastify_cors_1.default, {
         origin: "*",
@@ -52,7 +54,7 @@ async function registerPlugins(app, opts) {
 async function registerRoutes(app) {
     // Checks for file that ends in `.ts` or `.js`
     const validFileRegex = /\.js$|\.ts$/;
-    const FilesInRoutes = await fs_1.default.promises.readdir(path_1.default.resolve(__dirname, "./routes/"));
+    const FilesInRoutes = await promises_1.default.readdir(path_1.default.resolve(__dirname, "./routes/"));
     const routeFiles = FilesInRoutes.filter(file => validFileRegex.test(file));
     for (const routeFile of routeFiles) {
         const routeImport = await Promise.resolve().then(() => __importStar(require(`./routes/${routeFile}`)));
@@ -72,6 +74,13 @@ async function buildServer(app, opts) {
         await opts.prisma.$connect();
         await registerPlugins(app, opts);
         await registerRoutes(app);
+        app.addHook("onRequest", (req, res, done) => {
+            if (req.headers.authorization !== process.env.SERVER_AUTHKEY) {
+                res.status(401).send("401 Unauthorized: Provide API Key");
+                return;
+            }
+            done();
+        });
         return app;
     }
     catch (err) {
