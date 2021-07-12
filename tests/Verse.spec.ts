fe854860559e9f5be41785b23c9ec787edffd9fc
Angelo <70970878+redstripez08@users.noreply.github.com>
2021-07-13 02:07:53 +0800

    Updated POST and GET Iter test. Also renamed some tests

--- tests/Verse.spec.ts
+++ tests/Verse.spec.ts
@@ -2,7 +2,7 @@ import buildServer from "../src/server";
 import fastify, { FastifyInstance } from "fastify";
 import prisma from "../src/schema/PrismaClient";
 
-describe("Test /verse", () => {
+describe("Test /verse Endpoint", () => {
     let app: FastifyInstance;
 
     beforeAll(async () => {
@@ -18,7 +18,7 @@ describe("Test /verse", () => {
         return Promise.resolve();
     }, 300000);
 
-    it("Get all Verses", async () => {
+    it("GET /verse", async () => {
         const res = await app.inject({
             method: "GET",
             url: "/verse",
@@ -41,7 +41,47 @@ describe("Test /verse", () => {
         return Promise.resolve();
     });
 
-    it("Get specific verses", async () => {
+    it("POST /verse", async () => {
+        const res = await app.inject({
+            method: "POST",
+            url: "/verse",
+            headers: {
+                authorization: process.env.SERVER_AUTHKEY,
+            },
+            payload: {
+                id: 10,
+                title: "nice",
+                content: "broh",
+            },
+        });
+
+        const expected = expect.objectContaining({
+            id: expect.any(Number),
+            title: expect.any(String),
+            conttent: expect.any(String),
+        });
+
+        expect(res.statusCode).toEqual(200);
+        expect(JSON.parse(res.payload)).toEqual(expected);
+
+        return Promise.resolve();
+    });
+
+    it.todo("PUT /verse");
+    it.todo("PATCH /verse");
+    it.todo("DELETE /verse");
+
+    it("GET Should Fail Authentication", async () => {
+        const res = await app.inject({
+            method: "GET",
+            url: "/verse",
+        });
+
+        expect(res.statusCode).toEqual(401);
+        return Promise.resolve();
+    });
+
+    it("GET Iterate over /verses/:id", async () => {
         for (let i = 0; i < 10; i++) {
             const res = await app.inject({
                 method: "GET",
@@ -60,30 +100,6 @@ describe("Test /verse", () => {
             expect(res.statusCode).toEqual(200);
             expect(JSON.parse(res.payload)).toEqual(expected || null);
         }
-
-        return Promise.resolve();
-    });
-
-    it("Should fail auth", async () => {
-        const res = await app.inject({
-            method: "GET",
-            url: "/verse",
-        });
-
-        expect(res.statusCode).toEqual(401);
-        return Promise.resolve();
-    });
-
-    it.skip("POST verse", async () => {
-        const res = await app.inject({
-            method: "POST",
-            url: "/verse/2",
-            headers: {
-                authorization: process.env.SERVER_AUTHKEY,
-            },
-        });
-
-        expect(res.statusCode).toEqual(200);
 
         return Promise.resolve();
     });
