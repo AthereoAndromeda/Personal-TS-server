61d2fcbe19e1df493f9b5c124e2e249e76d0b7f9
Angelo <70970878+redstripez08@users.noreply.github.com>
2021-07-12 18:58:50 +0800

    Augment DB for fastify decorator

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,6 +1,13 @@
+import { PrismaClient } from "@prisma/client";
 import { FastifyPluginCallback } from "fastify";
 
 export interface Route {
     path: string;
     route: FastifyPluginCallback;
+}
+
+declare module "fastify" {
+    interface FastifyInstance {
+        db: PrismaClient;
+    }
 }
