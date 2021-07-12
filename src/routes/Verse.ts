10d4b4c8a04218425dc610e0390278e2596eedcf
Angelo <70970878+redstripez08@users.noreply.github.com>
2021-07-13 00:56:54 +0800

    Created POST endpoint and JSON schema

--- src/routes/Verse.ts
+++ src/routes/Verse.ts
@@ -1,9 +1,11 @@
+import { Static, Type } from "@sinclair/typebox";
 import {
     FastifyPluginCallback,
-    FastifyRequest,
-    onRequestHookHandler,
+    FastifySchema,
+    preHandlerHookHandler,
     RouteShorthandOptions,
 } from "fastify";
+import { IncomingMessage, Server, ServerResponse } from "http";
 import { Route } from "typings";
 
 interface Querystring {
@@ -16,27 +18,61 @@ interface Params {
     id: string;
 }
 
-interface ReqInterface extends FastifyRequest {
+interface ReqInterface {
     Querystring: Querystring;
     Params: Params;
+    Body: VerseType;
+    Response: VerseType;
 }
 
-const cb: onRequestHookHandler = (req, res, done) => {
-    if (req.headers.authorization !== process.env.SERVER_AUTHKEY) {
-        res.status(401).send("401 Unauthorized: Provide API Key");
+const Verse = Type.Object({
+    id: Type.Number(),
+    title: Type.String(),
+    content: Type.String(),
+});
+
+type CustomOpts = RouteShorthandOptions<
+    Server,
+    IncomingMessage,
+    ServerResponse,
+    ReqInterface,
+    unknown,
+    FastifySchema
+>;
+
+const postOpts: CustomOpts = {
+    schema: {
+        body: Verse,
+        response: {
+            200: Verse,
+        },
+    },
+    preHandler: (req, res, done) => {
+        const parsedParam = parseInt(req.params.id);
+
+        if (isNaN(parsedParam)) {
+            const errMsg = "400 Bad Request: Parameter must be a Number!";
+            res.status(400).header("content-type", "text/plain").send(errMsg);
+            return;
+        }
         done();
-    }
-
-    done();
+    },
 };
 
-const opt: RouteShorthandOptions = {
-    onRequest: cb,
-};
+type VerseType = Static<typeof Verse>;
 
 const route: FastifyPluginCallback = (app, opts, next) => {
+    app.addHook("preValidation", (req, res, done) => {
+        if (req.headers.authorization !== process.env.SERVER_AUTHKEY) {
+            res.status(401).send("401 Unauthorized: Provide API Key");
+            done();
+        }
+
+        done();
+    });
+
     // Returns all verses
-    app.get<ReqInterface>("/", opt, async (req, res) => {
+    app.get<ReqInterface>("/", async (req, res) => {
         try {
             const data = await app.db.verse.findMany();
 
@@ -48,13 +84,17 @@ const route: FastifyPluginCallback = (app, opts, next) => {
     });
 
     // Returns verse with matching id
-    app.get<ReqInterface>("/:id", opt, async (req, res) => {
+    app.get<ReqInterface>("/:id", async (req, res) => {
         try {
             const parsedParam = parseInt(req.params.id);
 
             if (isNaN(parsedParam)) {
                 const errMsg = "400 Bad Request: Parameter must be a Number!";
-                res.status(400).send(errMsg);
+
+                res.status(400)
+                    .header("content-type", "text/plain")
+                    .send(errMsg);
+
                 return;
             }
 
@@ -68,6 +108,33 @@ const route: FastifyPluginCallback = (app, opts, next) => {
         } catch (error) {
             app.log.error(error);
             res.status(500).send("500 Internal Server Error");
+        }
+    });
+
+    app.post<ReqInterface>("/", postOpts, async (req, res) => {
+        try {
+            const parsedParam = parseInt(req.params.id);
+
+            if (isNaN(parsedParam) || !req.params.id) {
+                const errMsg = "400 Bad Request: Parameter must be a Number!";
+
+                res.status(400)
+                    .header("content-type", "text/plain")
+                    .send(errMsg);
+
+                return;
+            }
+            const data = await app.db.verse.create({
+                data: {
+                    id: req.body.id,
+                    title: req.body.title,
+                    content: req.body.content,
+                },
+            });
+
+            res.status(200).send(data);
+        } catch (error) {
+            res.status(500).send(error);
         }
     });
 
