8007522437e88811ab17d95e7ab63ff7c0dc8223
Angelo <70970878+redstripez08@users.noreply.github.com>
2021-07-15 23:21:45 +0800

    Exported Verse type

--- src/schema/model.d.ts
+++ src/schema/model.d.ts
@@ -1,5 +1,6 @@
-export interface Verse {
-    id: number;
-    title: string;
-    content: string;
-}
+// This does not work for Nexus SourceTypes, Nexus won't detect it and won't appear in nexus-typgen
+// export { VerseType as Verse } from "./Verse";
+
+// This method does work though
+import type { VerseType } from "./Verse";
+export type Verse = VerseType;
