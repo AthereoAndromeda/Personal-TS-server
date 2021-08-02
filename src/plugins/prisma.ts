bc2c00ff75cb0cc8cd43deaa7f99cd22b570fc69
Angelo <70970878+redstripez08@users.noreply.github.com>
2021-08-02 19:04:32 +0800

    Added typings to PrismaPlugin

--- src/plugins/prisma.ts
+++ src/plugins/prisma.ts
@@ -2,14 +2,23 @@ import { PrismaClient } from "@prisma/client";
 import { FastifyPluginAsync } from "fastify";
 import fp from "fastify-plugin";
 
+declare module "fastify" {
+    interface FastifyInstance {
+        db: PrismaClient;
+    }
+}
+
 interface PrismaPluginOptions {
-    db: PrismaClient;
+    db?: PrismaClient;
 }
 
 const prismaPlugin: FastifyPluginAsync<PrismaPluginOptions> = fp(
     async (app, options) => {
-        app.decorate("db", options.db);
-        await app.db.$connect();
+        if (options.db) {
+            app.decorate("db", options.db);
+        } else {
+            app.decorate("db", new PrismaClient());
+        }
     }
 );
 
