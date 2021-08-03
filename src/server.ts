6531ccd472d1ca294bec78ef62494917f19749a7
Angelo <70970878+redstripez08@users.noreply.github.com>
2021-08-03 16:59:53 +0800

    Moved plugin order to fix tests

--- src/server.ts
+++ src/server.ts
@@ -7,28 +7,19 @@ import mercurius from "mercurius";
 import { Route } from "typings";
 import { checkNodeEnv } from "./utils";
 import { IncomingMessage, Server, ServerResponse } from "http";
-// import middiePlugin from "middie";
 import fastifyBlipp from "fastify-blipp";
 import fastifyHelmet from "fastify-helmet";
 import prismaPlugin from "./plugins/prisma";
 
 function registerPlugins(app: FastifyInstance) {
-    const ctx = {
-        db: app.db,
-    };
-
-    // app.register(fastifyGracefulShutdown);
-
     app.register(fastifyBlipp);
 
     app.register(fastifyHelmet);
 
-    // app.register(middiePlugin);
-
     app.register(mercurius, {
         schema: gqlSchema,
         graphiql: "playground",
-        context: () => ctx,
+        context: () => ({ db: app.db }),
     });
 
     app.register(fastifyCors, {
@@ -85,12 +76,11 @@ export default async function buildServer(
     app: FastifyInstance
 ): Promise<BuildReturn> {
     try {
+        registerCustomPlugins(app); // 2
+
         registerPlugins(app); // 1
 
-        registerCustomPlugins(app);
-
-        await registerRoutes(app); // 2
-        // app.decorate("db", opts.prisma); // 3
+        await registerRoutes(app); // 3
 
         return app;
     } catch (err) {
