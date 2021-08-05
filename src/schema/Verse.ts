f0255f8c4043508ce998db0e4c16455ea71bc6eb
Angelo <athereoandromeda@gmail.com>
2021-08-05 23:09:47 +0800

    Renamed `Verse` to `VerseSchema`

--- src/schema/Verse.ts
+++ src/schema/Verse.ts
@@ -1,9 +1,9 @@
 import { Type, Static } from "@sinclair/typebox";
 
-export const Verse = Type.Object({
+export const VerseSchema = Type.Object({
     id: Type.Number(),
     title: Type.String(),
     content: Type.String(),
 });
 
-export type VerseType = Static<typeof Verse>;
+export type VerseType = Static<typeof VerseSchema>;
