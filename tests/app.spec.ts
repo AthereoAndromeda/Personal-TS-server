47f7021b0e99bc062edd560fecce1aadaad726ee
Angelo <athereoandromeda@gmail.com>
2021-08-05 18:57:28 +0800

    Removed unnecessary returns

--- tests/app.spec.ts
+++ tests/app.spec.ts
@@ -22,15 +22,12 @@ describe("Test Authorization and Response", () => {
 
         await app.db.$connect();
         await app.listen(8081, "0.0.0.0");
-        return;
     }, 300000);
 
     // Stop Server and disconnect DB
     afterAll(async () => {
         await app.db.$disconnect();
         await app.close();
-
-        return;
     }, 300000);
 
     it("Should Return 200 Success to all Requests", async () => {
@@ -41,8 +38,6 @@ describe("Test Authorization and Response", () => {
 
         expect(res.statusCode).toEqual(200);
         expect(res.payload).toEqual(expect.anything());
-
-        return;
     });
 
     it("Should Return 401 Unauthorized: Incorrect Auth Key", async () => {
@@ -56,8 +51,6 @@ describe("Test Authorization and Response", () => {
 
         expect(res.statusCode).toEqual(401);
         expect(res.payload).toEqual(expect.anything());
-
-        return;
     });
 
     it("Should Return 401 Unauthorized: No Auth Key", async () => {
@@ -68,8 +61,6 @@ describe("Test Authorization and Response", () => {
 
         expect(res.statusCode).toEqual(401);
         expect(res.payload).toEqual(expect.anything());
-
-        return;
     });
 
     it("Should Return 200 Success: Correct Auth Key", async () => {
@@ -93,7 +84,5 @@ describe("Test Authorization and Response", () => {
 
         expect(res.statusCode).toEqual(200);
         expect(JSON.parse(res.payload)).toEqual(expectedValue);
-
-        return;
     });
 });
