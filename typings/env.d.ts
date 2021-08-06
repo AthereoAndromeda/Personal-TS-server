59fb468ad2b4a9a6555a45d670ae56bb545b2d22
Angelo <athereoandromeda@gmail.com>
2021-08-06 22:48:52 +0800

    Fixed env types from `SERVER_AUTHKEY` to `SERVER_AUTH`

--- typings/env.d.ts
+++ typings/env.d.ts
@@ -3,7 +3,7 @@ declare global {
         interface ProcessEnv {
             PORT?: string;
             NODE_ENV?: "development" | "production";
-            SERVER_AUTHKEY?: string;
+            SERVER_AUTH?: string;
         }
     }
 }
