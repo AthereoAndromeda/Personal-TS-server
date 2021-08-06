aef0968784cc8f9bd770d4236c972d6daf93f58b
Angelo <athereoandromeda@gmail.com>
2021-08-07 01:52:46 +0800

    Updated Context types

--- src/schema/context.d.ts
+++ src/schema/context.d.ts
@@ -1,5 +1,8 @@
 import { PrismaClient } from "@prisma/client";
+import { FastifyRequest, FastifyReply } from "fastify";
 
 export interface Context {
     db: PrismaClient;
+    req: FastifyRequest;
+    res: FastifyReply;
 }
