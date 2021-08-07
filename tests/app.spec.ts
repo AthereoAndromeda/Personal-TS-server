67afa0a67406e9442b5a497ff4d2190673c1f942
Angelo <athereoandromeda@gmail.com>
2021-08-07 16:39:37 +0800

    Moved all tests to this file

--- tests/app.spec.ts
+++ tests/app.spec.ts
@@ -1,8 +1,9 @@
 import buildServer, { BuildReturn } from "../src/server";
 import fastify from "fastify";
+import * as gql from "gql-query-builder";
 import { mockDeep } from "jest-mock-extended";
-import { PrismaClient } from "@prisma/client";
-import { DeepMockProxy } from "jest-mock-extended/lib/Mock";
+import { PrismaClient, Verse } from "@prisma/client";
+import { DeepMockProxy, mockReset } from "jest-mock-extended/lib/Mock";
 
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore Due to circular reference errors
@@ -10,8 +11,50 @@ interface MockServer extends BuildReturn {
     db: DeepMockProxy<PrismaClient>;
 }
 
-describe("Test Authorization and Response", () => {
+declare global {
+    // eslint-disable-next-line @typescript-eslint/no-namespace
+    namespace jest {
+        interface Matchers<R extends void | Promise<void>> {
+            /** Same as `toEqual()`, but allows null values*/
+            toEqualNullable<E = unknown>(a: E): R;
+        }
+    }
+}
+
+const okObject = {
+    message: () => "Ok",
+    pass: true,
+};
+
+expect.extend({
+    toEqualNullable(received, expected) {
+        if (received === null) {
+            return okObject;
+        } else if (this.equals(received, expected)) {
+            return okObject;
+        } else {
+            return {
+                message: () => `expected ${received} to be ${expected}`,
+                pass: false,
+            };
+        }
+    },
+});
+
+describe("Test App Endpoints", () => {
     let app: MockServer;
+    const verses: Verse[] = [
+        {
+            id: 1,
+            content: "Test Content",
+            title: "Test Title",
+        },
+        {
+            id: 2,
+            content: "Test Content 2",
+            title: "Test Title 2",
+        },
+    ];
 
     // Start Server and connect to DB
     beforeAll(async () => {
@@ -19,70 +62,201 @@ describe("Test Authorization and Response", () => {
         const prismaMock = mockDeep<PrismaClient>();
 
         app.db = prismaMock;
-
-        await app.db.$connect();
-        await app.listen(8081, "0.0.0.0");
-    }, 300000);
-
-    // Stop Server and disconnect DB
-    afterAll(async () => {
-        await app.db.$disconnect();
-        await app.close();
-    }, 300000);
-
-    it("Should Return 200 Success to all Requests", async () => {
-        const res = await app.inject({
-            method: "GET",
-            url: "/",
-        });
-
-        expect(res.statusCode).toEqual(200);
-        expect(res.payload).toEqual(expect.anything());
     });
 
-    it("Should Return 401 Unauthorized: Incorrect Auth Key", async () => {
-        const res = await app.inject({
-            method: "GET",
-            url: "/verses",
-            headers: {
-                authorization: "incorrectAuthorizationKey",
-            },
-        });
-
-        expect(res.statusCode).toEqual(401);
-        expect(res.payload).toEqual(expect.anything());
+    afterEach(() => {
+        mockReset(app.db);
     });
 
-    it("Should Return 401 Unauthorized: No Auth Key", async () => {
-        const res = await app.inject({
-            method: "GET",
-            url: "/verses",
-        });
+    describe("Test /", () => {
+        it("GET /", async () => {
+            const res = await app.inject({
+                method: "GET",
+                url: "/",
+            });
 
-        expect(res.statusCode).toEqual(401);
-        expect(res.payload).toEqual(expect.anything());
+            expect(res.statusCode).toBe(200);
+            expect(res.payload).toEqual(expect.anything());
+        });
     });
 
-    it("Should Return 200 Success: Correct Auth Key", async () => {
-        const expectedValue = [
-            {
-                id: 1,
-                title: "Test Title",
-                content: "Test Content",
-            },
-        ];
+    describe("Test /verses", () => {
+        const noAPIKey = {
+            statusCode: 401,
+            error: "Unauthorized",
+            message: "API Key Required",
+        };
 
-        app.db.verse.findMany.mockResolvedValue(expectedValue);
+        it("Return 401 Unauthorized: No Auth Key", async () => {
+            const res = await app.inject({
+                method: "GET",
+                url: "/verses",
+            });
 
-        const res = await app.inject({
-            method: "GET",
-            url: "/verses",
-            headers: {
-                authorization: process.env.SERVER_AUTH,
-            },
+            expect(res.statusCode).toBe(401);
+            expect(JSON.parse(res.payload)).toStrictEqual(noAPIKey);
         });
 
-        expect(res.statusCode).toEqual(200);
-        expect(JSON.parse(res.payload)).toEqual(expectedValue);
+        it("Return 401 Unauthorized: Incorrect Auth Key", async () => {
+            const res = await app.inject({
+                method: "GET",
+                url: "/verses",
+                headers: {
+                    authorization: "incorrectAuthorizationKey",
+                },
+            });
+
+            expect(res.statusCode).toBe(401);
+            expect(JSON.parse(res.payload)).toStrictEqual(noAPIKey);
+        });
+
+        it("GET /verses", async () => {
+            const expectedValue = verses;
+            app.db.verse.findMany.mockResolvedValue(expectedValue);
+
+            const res = await app.inject({
+                method: "GET",
+                url: "/verses",
+                headers: {
+                    authorization: process.env.SERVER_AUTH,
+                },
+            });
+
+            expect(res.statusCode).toBe(200);
+            expect(JSON.parse(res.payload)).toEqual(expectedValue);
+        });
+
+        it("POST /verses", async () => {
+            const expectedValue = verses[0];
+            app.db.verse.create.mockResolvedValue(expectedValue);
+
+            const res = await app.inject({
+                method: "POST",
+                url: "/verses",
+                headers: {
+                    authorization: process.env.SERVER_AUTH,
+                },
+                payload: expectedValue,
+            });
+
+            expect(res.statusCode).toBe(200);
+            expect(JSON.parse(res.payload)).toEqual(expectedValue);
+        });
+
+        it("PUT /verses", async () => {
+            const expectedValue = verses[0];
+            app.db.verse.update.mockResolvedValue(expectedValue);
+
+            const res = await app.inject({
+                method: "PUT",
+                url: "/verses",
+                headers: {
+                    authorization: process.env.SERVER_AUTH,
+                },
+                payload: expectedValue,
+            });
+
+            expect(res.statusCode).toBe(200);
+            expect(JSON.parse(res.payload)).toEqual(expectedValue);
+        });
+
+        it.todo("PATCH /verses");
+
+        it("DELETE /verses", async () => {
+            const expectedValue = verses[0];
+            const payload = { id: expectedValue.id };
+            app.db.verse.delete.mockResolvedValue(expectedValue);
+
+            const res = await app.inject({
+                method: "DELETE",
+                url: "/verses",
+                headers: {
+                    authorization: process.env.SERVER_AUTH,
+                },
+                payload,
+            });
+
+            expect(res.statusCode).toBe(200);
+            expect(JSON.parse(res.payload)).toEqual(expectedValue);
+        });
+
+        it("GET Iterate over /verses/:id", async () => {
+            app.db.verse.findUnique
+                .mockResolvedValueOnce(verses[0])
+                .mockResolvedValueOnce(verses[1])
+                .mockResolvedValue(null);
+
+            for (let i = 0; i < 3; i++) {
+                const expectedValue = verses[i] ? verses[i] : null;
+
+                const res = await app.inject({
+                    method: "GET",
+                    url: `/verses/${i}`,
+                    headers: {
+                        authorization: process.env.SERVER_AUTH,
+                    },
+                });
+
+                expect(res.statusCode).toBe(200);
+                expect(JSON.parse(res.payload)).toEqualNullable(expectedValue);
+            }
+        });
+    });
+
+    describe("Test /graphql", () => {
+        it("Queries All Verses", async () => {
+            const expectedValue = verses;
+            app.db.verse.findMany.mockResolvedValue(expectedValue);
+
+            const payload = gql.query({
+                operation: "verse",
+                fields: ["id", "title", "content"],
+            });
+
+            const res = await app.inject({
+                method: "POST",
+                url: "/graphql",
+                headers: {
+                    authorization: process.env.SERVER_AUTH,
+                },
+                payload,
+            });
+
+            expect(res.statusCode).toBe(200);
+            expect(JSON.parse(res.payload)).toEqual({
+                data: {
+                    verse: expectedValue,
+                },
+            });
+        });
+
+        it("Queries Specific Verse", async () => {
+            const expectedValue = verses[0];
+            app.db.verse.findUnique.mockResolvedValue(expectedValue);
+
+            const payload = gql.query({
+                operation: "verse",
+                variables: {
+                    id: 1,
+                },
+                fields: ["id", "title", "content"],
+            });
+
+            const res = await app.inject({
+                method: "POST",
+                url: "/graphql",
+                headers: {
+                    authorization: process.env.SERVER_AUTH,
+                },
+                payload,
+            });
+
+            expect(res.statusCode).toBe(200);
+            expect(JSON.parse(res.payload)).toEqual({
+                data: {
+                    verse: [expectedValue],
+                },
+            });
+        });
     });
 });
