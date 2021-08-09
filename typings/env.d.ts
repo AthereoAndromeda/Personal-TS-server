a38cb4b42e8dec77ddce19fd05ff19ea0c244522
Angelo <athereoandromeda@gmail.com>
2021-08-09 20:46:40 +0800

    Added `test` to `NODE_ENV`

--- typings/env.d.ts
+++ typings/env.d.ts
@@ -2,7 +2,7 @@ declare global {
     namespace NodeJS {
         interface ProcessEnv {
             PORT?: string;
-            NODE_ENV?: "development" | "production";
+            NODE_ENV?: "development" | "production" | "test";
             SERVER_AUTH?: string;
         }
     }
